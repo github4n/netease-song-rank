c699468ff02f81d66ef811798a5dd4c67935b120
vayci <vayci2012@163.com>
2018-12-25 16:28:06 +0800

    缓存健康检查

--- src/main/java/me/olook/netease/song/rank/rest/HealthController.java
+++ src/main/java/me/olook/netease/song/rank/rest/HealthController.java
@@ -2,9 +2,11 @@ package me.olook.netease.song.rank.rest;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import me.olook.netease.song.rank.cache.TemplateMessageCache;
 import me.olook.netease.song.rank.util.proxy.ProxyInfo;
 import me.olook.netease.song.rank.util.proxy.ProxyPoolUtil;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +25,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class HealthController {
 
     @ApiOperation(value = "代理池状态")
-    @GetMapping(value = "proxy")
+    @GetMapping(value = "proxy/info")
     public ResponseEntity proxy(){
         Map<String,Object> map = new HashMap<>(5);
         map.put("activeSize",ProxyPoolUtil.activeSize());
@@ -34,5 +36,18 @@ public class HealthController {
         map.put("workQueue",workQueue);
         map.put("failQueue",failQueue);
         return ResponseEntity.status(200).body(map);
+    }
+
+    @ApiOperation(value = "推送缓存状态")
+    @GetMapping(value = "cache/info")
+    public ResponseEntity cacheInfo(){
+        return ResponseEntity.status(200).body(TemplateMessageCache.info());
+    }
+
+    @ApiOperation(value = "清除推送缓存")
+    @DeleteMapping(value = "cache/clean")
+    public ResponseEntity cacheClean(){
+        TemplateMessageCache.cleanCache();
+        return ResponseEntity.status(200).body("");
     }
 }
--- src/main/java/me/olook/netease/song/rank/util/netease/NetEaseSignedSinger.java
+++ src/main/java/me/olook/netease/song/rank/util/netease/NetEaseSignedSinger.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Random;
+
 /**
  * @author zhaohw
  * @date 2018-12-17 16:26
@@ -23,4 +25,9 @@ public class NetEaseSignedSinger {
     public String toString() {
         return id+" : "+name+" : "+ rankNum;
     }
+
+    public String toSql(){
+        return "INSERT INTO `song_rank_job` VALUES (null, '1', '"+id+"', 'group2', '听歌排行爬取任务', '"+new Random().nextInt(60)+" */1 * * * ?', '"+id+"', '"+name+"', null, '2018-12-25 14:07:38', 'system', null, '2018-12-25 14:07:38', null, 'new job');";
+    }
+
 }
