032da3eef39850d4d80a9db9951fde565d7488af
vayci <vayci2012@163.com>
2019-01-24 13:17:15 +0800

    自定义QuartZ配置;jackson时区

---
+++ src/main/java/me/olook/netease/song/rank/config/CustomJobFactory.java
@@ -0,0 +1,29 @@
+package me.olook.netease.song.rank.config;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.scheduling.quartz.AdaptableJobFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zhaohw
+ * @date 2019-01-24 11:41
+ */
+@Component
+public class CustomJobFactory extends AdaptableJobFactory {
+
+    private final AutowireCapableBeanFactory capableBeanFactory;
+
+    @Autowired
+    public CustomJobFactory(AutowireCapableBeanFactory capableBeanFactory) {
+        this.capableBeanFactory = capableBeanFactory;
+    }
+
+    @Override
+    protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
+        Object jobInstance = super.createJobInstance(bundle);
+        capableBeanFactory.autowireBean(jobInstance);
+        return jobInstance;
+    }
+}
